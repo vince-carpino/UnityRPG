2dbd8247c16ea09ee5abcae86190679efedcf460
Vince Carpino <carpi111@mail.chapman.edu>
2020-02-10 17:13:04 -0800

    Adds EnemyController script and other related items

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    public float Health = 100f;
+
+    void Update() {
+        if (Health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag != "Bullet") {
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damage) {
+        Health -= damage;
+    }
+}
