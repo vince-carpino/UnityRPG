e369df695231ae706074b494f7a62ddf6679f45f
Vince Carpino <carpi111@mail.chapman.edu>
2020-02-11 17:02:20 -0800

    Adds explosion sphere to grenade

--- Assets/_Scripts/Grenade.cs
+++ Assets/_Scripts/Grenade.cs
@@ -1,6 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Grenade : MonoBehaviour {
+    private GameObject ExplosionSphere;
+
     public float Lifetime;
     public float DamageRadius;
     public float Damage;
@@ -23,15 +26,44 @@ public class Grenade : MonoBehaviour {
     }
 
     void Detonate() {
+        DrawExplosion();
+
         Collider[] collisions = Physics.OverlapSphere(transform.position, DamageRadius);
-        print(collisions.Length);
 
         foreach (var col in collisions) {
             if (col.gameObject.tag == "Enemy") {
                 col.GetComponent<EnemyController>().TakeDamage(Damage);
             }
         }
+    }
 
+    void DrawExplosion() {
+        ExplosionSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        ExplosionSphere.GetComponent<Collider>().isTrigger = true;
+        ExplosionSphere.transform.position = gameObject.transform.position;
+        StartCoroutine(ScaleOverTime(0.15f));
+    }
+
+    IEnumerator ExecuteAfterTime(float seconds) {
+        yield return new WaitForSeconds(seconds);
+
+        Destroy(ExplosionSphere);
+        Destroy(gameObject);
+    }
+
+    IEnumerator ScaleOverTime(float time) {
+        Vector3 originalScale = Vector3.zero;
+        Vector3 destinationScale = new Vector3(DamageRadius, DamageRadius, DamageRadius);
+
+        float currentTime = 0.0f;
+
+        do {
+            ExplosionSphere.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+
+        Destroy(ExplosionSphere);
         Destroy(gameObject);
     }
 
