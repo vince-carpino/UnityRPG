55c65a23285886ea6fe64507f6bac22162829533
Vince Carpino <carpi111@mail.chapman.edu>
2020-02-13 20:33:25 -0800

    Adds grenade countdown UI

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public Text TimerText;
+}
--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -2,8 +2,9 @@
 using UnityEngine;
 
 public class Weapon : MonoBehaviour {
-    private Vector3 LaunchVector;
     private bool _CanFire = true;
+    private float _Timer;
+    private GameManager GM;
 
     public GameObject ProjectilePrefab;
     public float LaunchSpeed;
@@ -12,12 +13,29 @@ public class Weapon : MonoBehaviour {
     private Rigidbody RB;
 
     void Start() {
+        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        GM.TimerText.text = string.Format("{0:0.0} s", CooldownTime);
         FirePoint = transform;
         RB = gameObject.transform.parent.GetComponent<Rigidbody>();
     }
 
+    void FixedUpdate() {
+        if (_CanFire) {
+            return;
+        }
+
+        _Timer -= Time.deltaTime;
+
+        if (_Timer <= 0f) { 
+            _Timer = CooldownTime;
+        }
+
+        GM.TimerText.text = string.Format("{0:0.0} s", _Timer);
+    }
+
     public void Fire() {
         _CanFire = false;
+        _Timer = CooldownTime;
         GameObject newProjectile = Instantiate(ProjectilePrefab, FirePoint.position, FirePoint.rotation);
         newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * LaunchSpeed + RB.velocity, ForceMode.Impulse);
         StartCoroutine(Cooldown());
