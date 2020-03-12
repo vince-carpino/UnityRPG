204934be156cdefb83d013b2e751ea8df7ead623
Vince Carpino <carpi111@mail.chapman.edu>
2020-03-12 13:13:28 -0700

    F Adds player sprint

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,9 +2,17 @@
 
 public class PlayerMovement : MonoBehaviour {
     [SerializeField]
+    private float originalForwardMoveSpeed = 7.5f;
+    [SerializeField]
+    private float originalBackwardMoveSpeed = 3f;
+    [SerializeField]
     private float forwardMoveSpeed = 7.5f;
     [SerializeField]
     private float backwardMoveSpeed = 3;
+    [SerializeField]
+    private float forwardSprintSpeed = 10f;
+    [SerializeField]
+    private float backwardSprintSpeed = 5f;
     [SerializeField]
     private float turnSpeed = 150;
 
@@ -14,6 +22,8 @@ public class PlayerMovement : MonoBehaviour {
     void Awake() {
         characterController = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
+        forwardMoveSpeed = originalForwardMoveSpeed;
+        backwardMoveSpeed = originalBackwardMoveSpeed;
     }
 
     void Update() {
@@ -30,5 +40,21 @@ public class PlayerMovement : MonoBehaviour {
             var moveSpeedToUse = vertical > 0 ? forwardMoveSpeed : backwardMoveSpeed;
             characterController.SimpleMove(transform.forward * moveSpeedToUse * vertical);
         }
+
+        ChangeMovementSpeedOnSprint();
+    }
+
+    private void ChangeMovementSpeedOnSprint() {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            forwardMoveSpeed = forwardSprintSpeed;
+            backwardMoveSpeed = backwardSprintSpeed;
+        } else {
+            forwardMoveSpeed = originalForwardMoveSpeed;
+            backwardMoveSpeed = originalBackwardMoveSpeed;
+        }
+    }
+
+    public void SetTurnSpeed(float speed) {
+        turnSpeed *= speed;
     }
 }
