9d9b959c01d2f8300558b7e2be9c65a0adaa61ae
Vince Carpino <carpi111@mail.chapman.edu>
2020-03-12 13:38:58 -0700

    F Adds skeleton quick menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -8,9 +8,20 @@ public class GameManager : MonoBehaviour {
     public static string FireAxisName = "Fire1";
     public static GameObject MainCamera;
     public static UIController UIController;
+    public static GameObject Player;
+
+    void Awake() {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Start() {
         MainCamera = GameObject.FindGameObjectWithTag(MainCameraTag).gameObject;
         UIController = GameObject.FindGameObjectWithTag(GameControllerTag).GetComponent<UIController>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            UIController.ToggleQuickMenu();
+        }
     }
 }
--- Assets/_Scripts/UIController.cs
+++ Assets/_Scripts/UIController.cs
@@ -5,9 +5,20 @@ public class UIController : MonoBehaviour {
     [SerializeField]
     private Text WeaponCooldownText = null;
 
+    [SerializeField]
+    private GameObject QuickMenu = null;
+
     private string PercentFormatString = "{0:0}%";
+
+    void Awake() {
+        QuickMenu.SetActive(false);
+    }
 
     public void SetWeaponCooldownText(string s) {
         WeaponCooldownText.text = string.Format(PercentFormatString, s);
+    }
+
+    public void ToggleQuickMenu() {
+        QuickMenu.SetActive(!QuickMenu.activeSelf);
     }
 }
